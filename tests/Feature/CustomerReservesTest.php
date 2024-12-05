f179d96500d5bd8372af72c38f7b8111279f752d
cathoen <c14220146@john.petra.ac.id>
2024-12-05 17:07:38 +0700

    feat: user search books n display no books testing

--- tests/Feature/CustomerReservesTest.php
+++ tests/Feature/CustomerReservesTest.php
@@ -6,6 +6,7 @@ use App\Models\Book;
 use App\Models\Customer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class CustomerReservesTest extends TestCase
@@ -29,6 +30,54 @@ class CustomerReservesTest extends TestCase
             'email' => $this->customer->email,
             'password' => 'password',
         ]);
+    }
+
+    /** @test */
+    public function it_displays_no_books_found_message_when_books_is_empty()
+    {
+        DB::table('books')->delete();
+
+        $response = $this->get(route('user.books'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('user.books');
+        $response->assertViewHas('books');
+        $response->assertViewHas('search');
+        $response->assertSee('Books');
+        $response->assertSee('No books found');
+    }
+
+    /** @test */
+    public function it_searches_for_books()
+    {
+        $book1 = Book::factory()->create(['name' => 'Book One']);
+        $book2 = Book::factory()->create(['name' => 'Book Two']);
+
+        $response = $this->get(route('user.books', ['search_book' => 'Book One']));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('user.books');
+        $response->assertViewHas('books');
+        $response->assertViewHas('search');
+        $response->assertSee('Books');
+        $response->assertSee($book1->name);
+        $response->assertDontSee($book2->name);
+    }
+
+    /** @test */
+    public function it_displays_no_books_found_message_when_searching_for_non_existent_book()
+    {
+        $book1 = Book::factory()->create(['name' => 'Book One']);
+        $book2 = Book::factory()->create(['name' => 'Book Two']);
+
+        $response = $this->get(route('user.books', ['search_book' => 'Book Three']));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('user.books');
+        $response->assertViewHas('books');
+        $response->assertViewHas('search');
+        $response->assertSee('Books');
+        $response->assertSee('No books found');
     }
 
     /** @test */
