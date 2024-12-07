67acba9382202155f99bd54e727fefb23af00d35
elizabethceline <elizabethceline04@gmail.com>
2024-12-07 14:48:02 +0700

    feat: user melihat detail buku test

--- tests/Feature/UserMelihatDetailBukuTest.php
+++ tests/Feature/UserMelihatDetailBukuTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Book;
 use App\Models\Customer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class UserMelihatDetailBukuTest extends TestCase
@@ -28,5 +30,37 @@ class UserMelihatDetailBukuTest extends TestCase
             'email' => $this->customer->email,
             'password' => 'password',
         ]);
+    }
+
+    /** @test */
+    public function it_displays_list_of_books()
+    {
+        $book1 = Book::factory()->create(['name' => 'Book One']);
+        $book2 = Book::factory()->create(['name' => 'Book Two']);
+
+        $response = $this->get(route('user.books'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('user.books');
+        $response->assertViewHas('books');
+        $response->assertViewHas('search');
+        $response->assertSee('Books');
+        $response->assertSee($book1->name);
+        $response->assertSee($book2->name);
+    }
+
+    /** @test */
+    public function it_displays_no_books_found_message_when_books_is_empty()
+    {
+        DB::table('books')->delete();
+
+        $response = $this->get(route('user.books'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('user.books');
+        $response->assertViewHas('books');
+        $response->assertViewHas('search');
+        $response->assertSee('Books');
+        $response->assertSee('No books found');
     }
 }
